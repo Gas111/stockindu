6037e4560ab5461c7f36f768add2d748c5e0456e
Gas111 <gastoncolque@gmail.com>
2022-08-13 23:57:31 -0300

    hola

--- stock/admin.py
+++ stock/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib import admin
+from stock.models import Persona,Vestimenta
+
+# Register your models here.
+
+class PersonaAdmin(admin.ModelAdmin):
+    list_display=("nombre","apellido","dni","celular","mail")
+    search_fields=("nombre","apellido","dni","celular","mail")
+
+
+class VestimentaAdmin(admin.ModelAdmin):
+    list_display=("ropa","zapatos","pulsera")
+    list_display=("ropa","zapatos","pulsera")
+
+
+admin.site.register(Persona,PersonaAdmin)
+admin.site.register(Vestimenta,VestimentaAdmin)
+
+admin.site.site_header = "Sitio web de Stock"
+admin.site.site_title = "Portal de Stock"
+admin.site.index_title = "Bienvenidos Web de Administración"
 
 # Register your models here.
--- stock/models.py
+++ stock/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre=models.CharField(max_length=10)
+    apellido=models.CharField(max_length=10)
+    dni=models.IntegerField(unique=True)
+    celular=models.IntegerField()
+    mail=models.EmailField(max_length=30,null=True,blank=True)
+    tallepantalon=models.IntegerField(null=True,blank=True)
+    tallezapatos=models.IntegerField(null=True,blank=True)
+    
+class Vestimenta(models.Model):
+    ropa=models.BooleanField(max_length=10)
+    zapatos=models.BooleanField(max_length=10)
+    pulsera=models.BooleanField(max_length=10)
+    
