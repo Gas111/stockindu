d422756c8a40f03829b4d2ea4c0eb958b584b1f6
Gas111 <gastoncolque@gmail.com>
2022-08-14 21:01:18 -0300

    Funciona todo - solo corregir base de datos detalles y falta el boostrap

--- stock/models.py
+++ stock/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Persona(models.Model):
-    nombre=models.CharField(max_length=10)
-    apellido=models.CharField(max_length=10)
-    dni=models.IntegerField(unique=True)
-    celular=models.IntegerField()
+    nombre=models.CharField(max_length=15)
+    apellido=models.CharField(max_length=15)
+    dni=models.PositiveIntegerField(unique=True)
+    celular=models.PositiveIntegerField()
     mail=models.EmailField(max_length=30,null=True,blank=True)
     tallepantalon=models.IntegerField(null=True,blank=True)
     tallezapatos=models.IntegerField(null=True,blank=True)
