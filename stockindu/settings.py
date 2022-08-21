1c2f97cd7a22c9e093fa160fc38a383a7a904d5f
Gas111 <gastoncolque@gmail.com>
2022-08-21 00:57:47 -0300

    mejoras en html

--- stockindu/settings.py
+++ stockindu/settings.py
@@ -79,6 +79,7 @@ WSGI_APPLICATION = 'stockindu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -89,7 +90,7 @@ DATABASES = {
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL')),
     }
-'''
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
