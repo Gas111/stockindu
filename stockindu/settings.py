1b2f05a6d114756729742b1dfe620e6c18fa666e
Gas111 <gastoncolque@gmail.com>
2023-03-19 11:14:00 -0300

    fix:

--- stockindu/settings.py
+++ stockindu/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3#ah5xvx=l)a9hy)!-9u^w7!xi!xw+)9crlbx+7rffwy%rjv@2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -80,18 +80,19 @@ WSGI_APPLICATION = 'stockindu.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 '''
 DATABASES = {
     'default': dj_database_url.config(default=config('DATABASE_URL')),
     }
-
+'''
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
